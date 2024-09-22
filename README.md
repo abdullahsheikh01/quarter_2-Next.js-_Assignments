b387924b75957d3356965a1e0030b1d8babb1c3c
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-09-22 23:05:59 +0500

    Project Two Completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # quarter_2-Next.js-_Assignments
-This is the repository of Assignments of Next.js project which is given by Sir Asharib Ali. 
+This is the repository of Assignments of Next.js project which is given by Sir [Asharib Ali](https://www.linkedin.com/in/asharibali/). 
---
+++ assignment_2/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Head from "../app/components/Header";// Name change to show the quality of export default
+import Aboutt from "../app/components/About";// Name change to show the quality of export default
+export default function Home(){
+  return(
+    <div>
+      <Head />
+      <h1>This is main page</h1>
+      <Aboutt />
+    </div>
+  )
+}
