80a6c39bc36377a0878dcbf583d190f40f78c237
abdullahsheikh01 <162831270+abdullahsheikh01@users.noreply.github.com>
2024-10-30 10:01:36 +0500

    resolving vercel error

--- assignment_7/src/app/pages/about_us/page.tsx
+++ assignment_7/src/app/pages/about_us/page.tsx
@@ -1,29 +1,20 @@
-export default function AboutUs(){
-    return(
+export default function AboutUs() {
+    return (
         <div className="flex flex-col justify-center items-center">
-            <h2 className="font-heading text-[68.3px]
-            text-white mt-7">About Us</h2>
-            <center>
-            <p className="text-white text-[25.3px]
-            font-paragraph ml-[121px] mr-[60px] tracking-wide 
-            leading-relaxed">
-            At Harmony Valley School, we are dedicated to 
-            fostering an enriching and supportive learning 
-            environment that empowers students to excel 
-            academically and personally. Our mission is to 
-            inspire a love for learning through a comprehensive 
-            curriculum, innovative teaching methods, and a 
-            strong emphasis on character development. With a 
-            diverse community of students and educators, we 
-            celebrate individuality and encourage collaboration, 
-            creativity, and critical thinking. We believe in 
-            nurturing each child's unique potential, preparing 
-            them to become responsible, compassionate, and engaged 
-            global citizens. At Harmony Valley, we are committed to 
-            cultivating harmony in education, ensuring every student 
-            feels valued and inspired to reach their dreams.
-            </p>
-            </center>
+            <h2 className="font-heading text-[68.3px] text-white mt-7">About Us</h2>
+            <div className="flex justify-center">
+                <p className="text-white text-[25.3px] font-paragraph mx-8 tracking-wide leading-relaxed">
+                    At Harmony Valley School, we are dedicated to fostering an enriching and supportive learning 
+                    environment that empowers students to excel academically and personally. Our mission is to 
+                    inspire a love for learning through a comprehensive curriculum, innovative teaching methods, 
+                    and a strong emphasis on character development. With a diverse community of students and 
+                    educators, we celebrate individuality and encourage collaboration, creativity, and critical 
+                    thinking. We believe in nurturing each child&apos;s unique potential, preparing them to become 
+                    responsible, compassionate, and engaged global citizens. At Harmony Valley, we are committed to 
+                    cultivating harmony in education, ensuring every student feels valued and inspired to reach 
+                    their dreams.
+                </p>
+            </div>
         </div>
-    )
+    );
 }
